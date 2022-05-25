778b769dcb723e3ac01cf90a1c50a2c7864ce22d
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-05-25 13:06:25 +0200

    Met data

--- form/src/app/app.module.ts
+++ form/src/app/app.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MaterialModule } from './material/material.module';
+import { AutocompleteFilterExampleComponent } from './autocomplete-filter-example/autocomplete-filter-example.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutocompleteFilterExampleComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
