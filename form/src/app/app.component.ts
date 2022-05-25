6f94f51c1600a5cd5e5bca612dd2e4038154a9bc
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-05-25 11:19:56 +0200

    Form

--- form/src/app/app.component.ts
+++ form/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'form';
+
+  @ViewChild('f') f = ElementRef
+
+  onSubmit()
+  {
+    console.log(this.f);
+  }
 }
--- form/src/app/app.module.ts
+++ form/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
