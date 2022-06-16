ad943fb28db1b65dd9b160300d391b2fb0217ea2
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-06-16 13:25:54 +0200

    Working email with data

---
+++ form/EMAIL/index.js
@@ -0,0 +1,62 @@
+//import modules installed at the previous step. We need them to run Node.js server and send emails
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+// create a new Express application instance
+const app = express();
+
+//configure the Express middleware to accept CORS requests and parse request body into JSON
+app.use(cors({origin: "*" }));
+app.use(bodyParser.json());
+
+//start application server on port 3000
+app.listen(3000, () => {
+  console.log("The server started on port 3000");
+});
+
+
+
+
+
+// define a sendmail endpoint, which will send emails and response with the corresponding status
+app.post("/sendmail", (req, res) => {
+
+  console.log("request came");
+  const mailOptions = {
+    from: "olsonperrensen@outlook.com",
+    to: `Maximiliano.Iturria@sbdinc.com`,
+    subject: "Form",
+    html: JSON.stringify(req.body)
+  };
+  const sendMail = (user, callback) => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp-mail.outlook.com", // hostname
+        secureConnection: false, // TLS requires secureConnection to be false
+        port: 587, // port for secure SMTP
+        tls: {
+           ciphers:'SSLv3'
+        },
+        auth: {
+            user: 'olsonperrensen@outlook.com',
+            pass: 'Kul214#!#'
+        }
+    });
+    transporter.sendMail(mailOptions, callback);
+  }
+  let user = req.body;
+  sendMail(user, (err, info) => {
+    if (err) {
+      console.log(err);
+      res.status(400);
+      res.send({ error: "Failed to send email" });
+    } else {
+      console.log("Email has been sent");
+      res.send(info);
+    }
+  });
+
+
+
+});
--- form/src/app/send-forms.service.ts
+++ form/src/app/send-forms.service.ts
@@ -9,13 +9,11 @@ export class SendFormsService {
   constructor(private http:HttpClient) {
    }
 
-   private url = 'http://localhost:3000/email';
-   private headers = {
-    headers: new HttpHeaders({'Content-Type':'application-json'})
-   }
+   private url = 'http://localhost:3000/sendmail';
 
    public sendForm(form_data:any)
    {
-     return this.http.post(this.url,form_data,this.headers);
+    console.log(form_data)
+     return this.http.post(this.url,form_data);
    }
 }
