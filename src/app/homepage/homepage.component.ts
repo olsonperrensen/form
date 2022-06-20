b2e74e29b2c442ab8e30cde7d21813ee2ecff8eb
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-06-20 14:09:42 +0200

    homepage fade in anim

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import * as a from 'angular-animations'
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  animations:[
+    a.fadeInLeftBigOnEnterAnimation()
+  ]
 })
 export class HomepageComponent implements OnInit {
+
 
   constructor() { }
 
