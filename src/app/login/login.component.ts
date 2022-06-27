c9b3a9bd0e37ea549771d41e6d3d6ddab39e68f2
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-06-27 16:38:49 +0200

    Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import * as CryptoJS from 'crypto-js';  
+import * as a from 'angular-animations';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations:[
+    a.bounceInLeftOnEnterAnimation(),
+    a.bounceInRightOnEnterAnimation(),
+    a.fadeInOnEnterAnimation(),
+    a.flipOnEnterAnimation(),
+  ]
 })
 export class LoginComponent implements OnInit {
+
+  isLoggedIn:boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSubmit(f:NgForm)
+  {
+    const secret = CryptoJS.AES.encrypt(JSON.stringify(f.value),'nghimax').toString();
+    console.log(secret);
+    // console.log(CryptoJS.AES.decrypt(secret,'nghimax').toString(CryptoJS.enc.Utf8));
+    
 
+    this.isLoggedIn=true;
+  }
 }
