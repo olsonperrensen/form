09fb99633096c2e22c6d6de046f902e992a824f2
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-06-27 16:33:16 +0200

    Vendor mockup

--- src/app/vendor/vendor.component.ts
+++ src/app/vendor/vendor.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as a from 'angular-animations';
 
 @Component({
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
-  styleUrls: ['./vendor.component.css']
+  styleUrls: ['./vendor.component.css'],
+  animations:[
+    a.fadeInLeftOnEnterAnimation()]
 })
 export class VendorComponent implements OnInit {
+
+  v_klant = ""
+  v_adres = ""
+  v_email = ""
+  v_gsm = ""
+  v_vat = ""
+  v_contact = ""
+  v_klantnr = ""
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(f:NgForm)
+  {
+    
+  }
+  pdfInputChange(e:Event)
+  {
+    
+  }
 }
