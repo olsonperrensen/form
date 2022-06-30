335f0e99d8ed5fc20b52bc3636ea87d62a8e4ab3
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-06-30 15:32:48 +0200

    .py and hardcoded str

---
+++ postgre.py
@@ -0,0 +1,11 @@
+import os
+import psycopg2
+
+DATABASE_URL = 'postgres://bteurqgnjifirr:8141b84bb364c480b8ab64f567d7b24b735e21e3cd285dcc210fbecf7d7195d3@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/d4l04oh9cth6jn'
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+c = conn.cursor()
+
+results = c.execute('SELECT * FROM BIZ')
+print(results)
