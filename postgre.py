bbd2d69d4266b9517d8d3de50cac5f5ff7993ce7
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-06-30 16:07:42 +0200

    Records inserted!

--- postgre.py
+++ postgre.py
@@ -6,11 +6,16 @@ DATABASE_URL = 'postgres://bteurqgnjifirr:8141b84bb364c480b8ab64f567d7b24b735e21
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 cursor = conn.cursor()
+
+file = open('clients.csv')
+clients = file.read().split(',')
+for client in clients:
+    sql_client = client.strip()
+    cursor.execute("INSERT INTO BIZ(biz_name) VALUES({})".format('\''+sql_client+'\''))
+
   
-sql = '''SELECT * FROM BIZ;'''
-  
-cursor.execute(sql)
-cursor.fetchone()
+
+# cursor.fetchone()
   
 conn.commit()
 cursor.close()
