485a36cee16779c405a23655692f6087abe4f9f6
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-07-04 15:14:17 +0200

    Formidable File parser implemented for PDF.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const nodemailer = require("nodemailer");
 const CryptoJS = require('crypto-js');
 require('dotenv').config();
 const fs = require('fs');
+const formidable = require('formidable');
 
 // create a new Express application instance
 const app = express();
@@ -48,10 +49,6 @@ app.get('/clients', (req, res) => {
   nieuw_clients = []
 });
 app.get('/sendmail', (req, res) => res.send("Send me a JSON object via POST. (Works with Zoho now)."));
-app.post('/vendor',(req,res)=>{
-  console.log(`Vendor request came ${req.body.v_file}`)
-  res.send({ant:req.body.v_file})
-})
 app.get('/vendor', (req, res) => res.send("Send me a Vendor object via POST. (Works with Zoho now)."));
 
 // define a sendmail endpoint, which will send emails and response with the corresponding status
@@ -236,3 +233,12 @@ app.delete('/clients', (req, res) => {
 
 
 })
+
+app.post('/vendor', (req, res) => {
+  const form = formidable({ multiples: true });
+  form.parse(req, (err, fields, files) => {
+      console.log('fields: ', fields);
+      console.log('files: ', files);
+      res.send({ success: true });
+  });
+});
