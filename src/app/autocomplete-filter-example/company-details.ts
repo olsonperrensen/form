1b9ce63b445b2e0cb6ad6b70cb13196d8f24cb8b
olsonperrensen <80784184+olsonperrensen@users.noreply.github.com>
2022-07-12 13:43:40 +0200

    Delete company-details.ts

--- src/app/autocomplete-filter-example/company-details.ts
+++
@@ -1,9 +0,0 @@
-export class CompanyDetails {
-    company_number!: string;
-    company_country!:string;
-    company_name!:string;
-    company_city!:string;
-    company_zip!:string;
-    company_street!:string;
-    company_vendor_id!:string;
-}
