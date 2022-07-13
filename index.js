ce08aa01cf924d14ba51125cf7e3bf721d88462e
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-07-13 11:36:44 +0200

    Rollback w/o pg_read_binary_file fn

--- index.js
+++ index.js
@@ -541,7 +541,7 @@ app.post('/vendor', upload.single('v_file'), (req, res) => {
         '${req.body.v_vat}',
         '${req.body.v_contact}',
         '${req.body.v_klantnr}',
-        pg_read_binary_file('${req.file.path}'),
+        '${req.file}',
         '${'Pending'}')`,
     (err, res) => {
       if (err) {
