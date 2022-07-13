ccf2ea703a65737e062f02b312fe60bec10afee7
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-07-13 11:40:30 +0200

    filename into db

--- index.js
+++ index.js
@@ -541,7 +541,7 @@ app.post('/vendor', upload.single('v_file'), (req, res) => {
         '${req.body.v_vat}',
         '${req.body.v_contact}',
         '${req.body.v_klantnr}',
-        '${req.file}',
+        '${req.file.originalname}',
         '${'Pending'}')`,
     (err, res) => {
       if (err) {
@@ -551,7 +551,7 @@ app.post('/vendor', upload.single('v_file'), (req, res) => {
       else {
         isRecordInDB = true;
         console.log(`record PO inserted #${external_id}}`)
-        console.log(res)
+        console.log(res.rowCount)
       }
     }
   );
