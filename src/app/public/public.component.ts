8da1bd7851f9d839e006d7d0578ad31bc5ce5b52
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-07-19 15:53:03 +0200

    public wall

---
+++ src/app/public/public.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as CryptoJS from 'crypto-js';
+import * as a from 'angular-animations';
+import { Route, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-public',
+  templateUrl: './public.component.html',
+  styleUrls: ['./public.component.css'],
+  animations: [
+    a.bounceInLeftOnEnterAnimation(),
+    a.bounceInRightOnEnterAnimation(),
+    a.fadeInOnEnterAnimation(),
+    a.flipOnEnterAnimation(),
+  ]
+})
+export class PublicComponent implements OnInit {
+
+  isLoggedIn: boolean = false;
+  isInvalid = false;
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(f: NgForm) {
+    const secret = CryptoJS.AES.encrypt(JSON.stringify(f.value), 'h#H@k*Bjp3SrwdLM').toString();
+    console.log(secret)
+    this.authService.isAuthenticated({ usr: secret }).subscribe((res) => {
+      console.log(res)
+      if (res === true) {
+        this.onLogin()
+        this.isLoggedIn = true;
+
+        setTimeout(() => {
+          this.router.navigate(['/', 'homepage'])
+        }, 2000);
+      }
+      else {
+        this.onLogout()
+        this.isLoggedIn = false;
+        this.isInvalid = true;
+        setTimeout(() => {
+          this.router.navigate(['/'])
+        }, 2000);
+      }
+    })
+  }
+
+  onLogin() {
+    this.authService.setGuardStatus(true)
+    this.authService.login()
+  }
+  onLogout() {
+    this.authService.logout()
+  }
+
+}
