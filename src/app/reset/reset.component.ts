8f6a1ade316d78d7a4f676f14c8fc81e1bb62eef
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-07-26 14:05:14 +0200

    reset with popup

--- src/app/reset/reset.component.ts
+++ src/app/reset/reset.component.ts
@@ -20,14 +20,22 @@ export class ResetComponent implements OnInit {
   u_id = 0;
   constructor(private authService: AuthService, private route: ActivatedRoute) { }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.u_id = this.route.snapshot.queryParams['id'];
-   }
+  }
   onSubmit(f: NgForm) {
     console.log(`Requesting from ID ${this.u_id}`)
     console.log(f.value.password)
-    this.authService.resetPWD(this.u_id,f.value.password).subscribe((res)=>{
-
-    })
+    f.value.password.length >= 4 ? this.authService.resetPWD(this.u_id, f.value.password).subscribe((res: any) => {
+      if (res.status == 200) {
+        alert("Password has been successfully changed! You can now log in.")
+      }
+      else if (res.status == 500) {
+        alert("ERROR. Password could NOT be changed!")
+      }
+      else {
+        alert("Something unexpected occurred. Contact students.benelux@sbdinc.com for further assistance.")
+      }
+    }) : alert("Password must be at least 4 characters long.");
   }
 }
