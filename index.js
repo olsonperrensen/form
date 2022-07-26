41ef408764beda368b3ba92d64ea2334b6aa7637
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-07-26 11:12:19 +0200

    debug login

--- index.js
+++ index.js
@@ -341,7 +341,7 @@ app.post('/login', (req, res) => {
     `select id from users where username = '${tmp_credentials[3]}'
     and password = '${tmp_credentials[7]}'`,
     (err, res) => {
-      console.log(console.rows[0])
+      console.log(console.rows)
     }
   );
   setTimeout(() => {
