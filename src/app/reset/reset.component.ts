afa53b6bdb7e6d3ce31a94e3372f27bc80d8ff0c
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-07-26 13:29:49 +0200

    reset styling

--- src/app/reset/reset.component.ts
+++ src/app/reset/reset.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as a from 'angular-animations';
 
 @Component({
   selector: 'app-reset',
   templateUrl: './reset.component.html',
-  styleUrls: ['./reset.component.css']
+  styleUrls: ['./reset.component.css'],
+  animations: [
+    a.bounceInLeftOnEnterAnimation(),
+    a.bounceInRightOnEnterAnimation(),
+    a.fadeInOnEnterAnimation(),
+    a.flipOnEnterAnimation(),
+  ]
 })
 export class ResetComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  onSubmit(f: NgForm) {
+    console.log(f.value)
+  }
 }
