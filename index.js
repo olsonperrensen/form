fe13fa927ece0536ab938bcaa85622deb6f6424b
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-08-09 11:43:22 +0200

    formatting po

--- index.js
+++ index.js
@@ -717,8 +717,7 @@ app.put('/po', (req, res) => {
       cc: `students.benelux@sbdinc.com`,
       subject: `PO #${req.body.u_ID} ${po_shortxt} ${tmp_company_po[1]} ${tmp_company_po[2]}`,
       html: `
-      <p class=MsoNormal>PO <b><span style='font-size:13.5pt;font-family:"Arial",sans-serif;color:navy'>${req.body.new_client}<o:p></o:p></span></b></p><p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><span lang=NL>Met vriendelijke groeten<o:p></o:p></span></p><p class=MsoNormal><span lang=NL><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span lang=NL>Students BeNeLux.-<o:p></o:p></span></p><p class=MsoNormal>Sales &amp; Marketing Support<o:p></o:p></p><p class=MsoNormal><o:p>&nbsp;</o:p></p>
-      <br><br>
+      <p class=MsoNormal>PO <b><span style='font-size:13.5pt;font-family:"Arial",sans-serif;color:navy'>${req.body.new_client}<o:p></o:p></span></b></p><p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><span lang=NL>Met vriendelijke groeten<o:p></o:p></span></p><p class=MsoNormal><span lang=NL><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span lang=NL>Students Benelux<o:p></o:p></span></p>
       <hr>
       <ul>Requested by: ${po_guy}</ul>
       <ul>Timestamp: ${po_datum}</ul>
