c24a5701d3c17fd3d3e190563d552c8966051fb1
Iturria <Maximiliano.Iturria@sbdinc.com>
2023-02-03 11:30:12 +0100

    endpoint split

--- src/app/send-forms.service.ts
+++ src/app/send-forms.service.ts
@@ -6,14 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class SendFormsService {
 
-  constructor(private http:HttpClient) {
-   }
+  constructor(private http: HttpClient) {
+  }
 
-   private url = 'https://formemail.herokuapp.com/sendmail';
+  private url = 'https://formemail.herokuapp.com/sendmail';
+  private url_split = 'https://formemail.herokuapp.com/sendmailsplit';
 
-   public sendForm(form_data:any)
-   {
+  public sendForm(form_data: any) {
     console.log(form_data)
-     return this.http.post(this.url,form_data);
-   }
+    if (form_data.bedrag_2 != '' && form_data.merk_2 != '') return this.http.post(this.url_split, form_data)
+    else return this.http.post(this.url, form_data)
+  }
 }
