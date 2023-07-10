81afd9a1510b61eaa6ca06e141df1cb307fe1912
Iturria <Maximiliano.Iturria@sbdinc.com>
2023-07-10 16:39:09 +0200

    werkende PO herkenning

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, UploadFile, File
+import easyocr
+
+app = FastAPI()
+reader = easyocr.Reader(['nl', 'fr', 'en'])
+
+
+@app.post("/")
+async def read_root(file: UploadFile = File(...)):
+    contents = await file.read()
+    res = reader.readtext(contents, detail=0, paragraph=True)
+    my_set = set(res)
+    substring = "450"
+    matching_elements = [element for element in my_set if substring in element]
+    if len(matching_elements) > 0:
+        return {"result": matching_elements}
