4ed49ef02fb170c3fe26a6fcf12aeef623dcc358
unknown <olson>
2023-07-11 15:19:16 +0200

    rewrite working

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import cv2
-import pytesseract
+from fastapi import FastAPI, UploadFile, File
+import cv2,pytesseract, numpy as np
 
-# Load the image using OpenCV
-image = cv2.imread('sample.png')
+app = FastAPI()
 
-# Preprocess the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+@app.post("/")
+async def read_root(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-# Perform OCR using Tesseract
-text = pytesseract.image_to_string(gray)
 
-# Print or use the extracted text as needed
-print(text)
+    # Preprocess the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    # Perform OCR using Tesseract
+    text = pytesseract.image_to_string(gray)
+
+    # Print or use the extracted text as needed
+    print(text)
