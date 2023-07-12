0491a0d808edf00ef93245e2914d81642456a657
unknown <olson>
2023-07-12 12:05:21 +0200

    returning green PO box

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-from fastapi import FastAPI, UploadFile, File
-import cv2,pytesseract, numpy as np
+from fastapi import FastAPI, UploadFile, File,Response
+from pytesseract import Output
+import cv2,pytesseract, re, numpy as np
 
 app = FastAPI()
 
 @app.post("/")
 async def read_root(file: UploadFile = File(...)):
-    contents = await file.read()
-    nparr = np.frombuffer(contents, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    try:
+        fximg = None
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        rawimg = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
+        d = pytesseract.image_to_data(rawimg, output_type=Output.DICT)
 
-    # Preprocess the image
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        date_pattern = r'^450[0-9]{7}$'
 
-    # Perform OCR using Tesseract
-    text = pytesseract.image_to_string(gray).split()
+        n_boxes = len(d['text'])
+        for i in range(n_boxes):
+            if int(d['conf'][i]) > 60:
+                if re.match(date_pattern, d['text'][i]):
+                    (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                    fximg = cv2.rectangle(rawimg, (x-7, y-7), (x + w+7, y + h+7), (0, 255, 0), 2)
 
-    # Print or use the extracted text as needed
-    my_set = set(text)
-    substring = "450"
-    matching_elements = [element for element in my_set if substring in element]
-    if len(matching_elements) > 0:
-        return {"result": matching_elements}
+        _, img_encoded = cv2.imencode('.png', fximg)
+        img_bytes = img_encoded.tobytes()
+
+        return Response(content=img_bytes, media_type="image/png")
+
+    except Exception as e:
+        return {"error": str(e)}
