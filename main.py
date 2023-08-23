f638fcbe9c880f51aa3cb4643ccbb9c05d842b3f
olsonperrensen <r0834721@student.thomasmore.be>
2023-08-23 11:43:25 +0200

    combined codes

--- main.py
+++ main.py
@@ -142,10 +142,9 @@ async def factuurnr(file:UploadFile=File(...)):
         found_nr = 0
 
         # Direct matches
-        SANTENS = r"VF\d{8}"
+        SANTENS_OF_ASAMCO = r"VF[A-Z]?\d{8}"
         CRESPIN = r"FV\d{9}"
         DE_DONCKER = r"VVF\d{2}/\d{6}"
-        ASAMCO = r"VFA\d{8}"
         NIJHOF = r"N\d{7}"
         # Indirect matches
         LECOT = r"V2$"
@@ -167,7 +166,7 @@ async def factuurnr(file:UploadFile=File(...)):
                     _, img_encoded = cv2.imencode('.jpeg', resized_img)
                     img_bytes = img_encoded.tobytes()
                     unix_time = int(time.time())
-            if re.match(SANTENS, d['text'][i]) or re.match(CRESPIN, d['text'][i]) or re.match(DE_DONCKER, d['text'][i])  or re.match(ASAMCO, d['text'][i]) or re.match(NIJHOF, d['text'][i]):
+            if re.match(SANTENS_OF_ASAMCO, d['text'][i]) or re.match(CRESPIN, d['text'][i]) or re.match(DE_DONCKER, d['text'][i]) or re.match(NIJHOF, d['text'][i]):
                 # FOUND DIRECT
                 print(f"found direct NR with value: {d['text'][i]}")
                 found_nr = d['text'][i]
