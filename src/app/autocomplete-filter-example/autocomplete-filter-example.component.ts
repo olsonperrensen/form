280dea5920ce6e869df4d37c8dc538a825a90360
olsonperrensen <r0834721@student.thomasmore.be>
2023-12-07 10:04:01 +0100

    fix van

--- src/app/autocomplete-filter-example/autocomplete-filter-example.component.ts
+++ src/app/autocomplete-filter-example/autocomplete-filter-example.component.ts
@@ -201,7 +201,7 @@ export class AutocompleteFilterExampleComponent implements OnInit {
     "Adriaan Arkeraats", "Arno DeJager", "Duncan DeWith", "Danielle Penninckx"];
   stanley_employees = ["Ken Leysen", "Martin Van Werkhoven", "Paul Kerkhoven", "Cedric Bicque",
     "Christian Fonteyn", "KlaasJan Bosgraaf", "Ammaar Basnoe", "Robert VanStraten", "Sven Pieters",
-    "Niek Nijland", "Robby Cattellion","Bert Van Bael"];
+    "Niek Nijland", "Robby Cattellion","Bert VanBael"];
 
   filteredOptions!: Observable<string[]>;
   filteredOptions2!: Observable<string[]>;
@@ -314,7 +314,7 @@ export class AutocompleteFilterExampleComponent implements OnInit {
           case "Robby Cattellion":
             this.u_land = "België / Belgique"
             break;
-          case "Bert Van Bael":
+          case "Bert VanBael":
           this.u_land = "België / Belgique"
           break;
           case "Marcel VandenBerge":
