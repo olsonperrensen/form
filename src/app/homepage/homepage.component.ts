252f83e7e1074487bbfba427c2183ef19b6c0def
olsonperrensen <r0834721@student.thomasmore.be>
2024-01-16 16:26:23 +0100

    Logout functionality.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -21,7 +21,24 @@ export class HomepageComponent implements OnInit {
   ngOnInit(): void {
     document.body.style.backgroundImage = "url('https://i.postimg.cc/8NqcDrfY/Default-Wallpaper.png')";
   }
-  goBack(): void {
-    this.location.back();
+  logOut(): void {
+    var cookies = document.cookie.split(";");
+
+    // Loop through each cookie and delete it
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+    //this.location.back();
+    // Clear local storage
+    localStorage.clear();
+
+    // Clear session storage
+    sessionStorage.clear();
+
+    // Reload the page
+    location.reload();
   }
 }
