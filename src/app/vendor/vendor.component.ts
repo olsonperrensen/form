8ba1b976bc7e3809b23af3dd800177ff46af3ed6
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-04-11 12:06:46 +0200

    redesigned vendor form

--- src/app/vendor/vendor.component.ts
+++ src/app/vendor/vendor.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, NgModel } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../auth.service';
@@ -11,60 +10,48 @@ import { Location } from '@angular/common';
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
   styleUrls: ['./vendor.component.css'],
-  
 })
 export class VendorComponent implements OnInit {
   isFormInvalid = false;
   isFormValidWithFile = false;
-  v_klant = ""
-  v_adres = ""
-  v_email = ""
-  v_gsm = ""
-  v_vat = ""
-  v_contact = ""
-  v_klantnr = ""
+  v_klant = '';
+  v_adres = '';
+  v_email = '';
+  v_gsm = '';
+  v_vat = '';
+  v_contact = '';
+  v_klantnr = '';
+  v_estimated_annual_spend = '';
   v_file!: File;
-  v_worker = '';
   myJSONForm = {};
   selected_file!: File;
-  res !: Res;
+  res!: Res;
   isBezig = false;
-  
 
-  constructor(private router: Router, private sendVendors: SendVendorsService,
-    private location: Location,private authService: AuthService) { }
+  constructor(
+    private router: Router,
+    private sendVendors: SendVendorsService,
+    private location: Location,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.v_worker = this.authService.getCredentials().naam
-    document.body.style.backgroundImage = "url('https://u.cubeupload.com/olsonperrensen2/313skyscraperwallpaperu.jpg')"
+    document.body.style.backgroundImage =
+      "url('https://u.cubeupload.com/olsonperrensen2/313skyscraperwallpaperu.jpg')";
   }
-  async onSubmit(f: NgForm) {
-    this.myJSONForm = {
-      v_klant: this.v_klant,
-      v_adres: this.v_adres,
-      v_email: this.v_email,
-      v_gsm: this.v_gsm,
-      v_vat: this.v_vat,
-      v_contact: this.v_contact,
-      v_klantnr: this.v_klantnr,
-      v_file: this.selected_file,
-      v_worker: this.v_worker
-    };
-
+  async onSubmit() {
     if (
       this.v_klant.length > 2 &&
       this.v_adres.length > 4 &&
       this.v_email.length > 4 &&
       this.v_vat.length > 4 &&
-      this.v_contact.length > 4 &&
-      this.v_worker.length > 4
+      this.v_contact.length > 4
     ) {
       if (
-        this.v_email.includes("@") &&
-        this.v_email.includes(".") &&
-        this.v_contact.includes(" ") &&
-        this.v_adres.includes(" ") &&
-        this.v_worker.includes(" ")
+        this.v_email.includes('@') &&
+        this.v_email.includes('.') &&
+        this.v_contact.includes(' ') &&
+        this.v_adres.includes(' ')
       ) {
         const fd = new FormData();
         fd.append('v_klant', this.v_klant);
@@ -72,22 +59,23 @@ export class VendorComponent implements OnInit {
         fd.append('v_email', this.v_email);
         fd.append('v_gsm', this.v_gsm);
         fd.append('v_vat', this.v_vat);
+        fd.append('v_estimated_annual_spend', this.v_estimated_annual_spend);
         fd.append('v_contact', this.v_contact);
         fd.append('v_klantnr', this.v_klantnr);
         fd.append('v_file', this.selected_file, this.selected_file.name);
-        fd.append('v_worker', this.v_worker);
+        fd.append('v_worker', this.authService.getCredentials().naam);
 
         try {
           this.isBezig = true;
           const res = await this.sendVendors.sendVendor(fd).toPromise();
           this.res = <Res>res;
-          if (this.res.response === "250 Message received") {
-            alert("Vendor aanvraag gestuurd!");
+          if (this.res.response === '250 Message received') {
+            alert('Vendor aanvraag gestuurd!');
             setTimeout(() => {
               this.router.navigate(['/']);
             }, 400);
           } else {
-            alert("Er ging iets mis.");
+            alert('Er ging iets mis.');
           }
           this.isBezig = false;
         } catch (err) {
@@ -104,15 +92,15 @@ export class VendorComponent implements OnInit {
     }
   }
 
-
   onCancel() {
-    this.v_klant = ""
-    this.v_adres = ""
-    this.v_email = ""
-    this.v_gsm = ""
-    this.v_vat = ""
-    this.v_contact = ""
-    this.v_klantnr = ""
+    this.v_klant = '';
+    this.v_adres = '';
+    this.v_email = '';
+    this.v_gsm = '';
+    this.v_vat = '';
+    this.v_contact = '';
+    this.v_klantnr = '';
+    this.v_estimated_annual_spend = '';
     setTimeout(() => {
       this.router.navigate(['/']);
     }, 1400);
@@ -120,7 +108,7 @@ export class VendorComponent implements OnInit {
 
   onFileSelected(event: any) {
     this.isFormValidWithFile = true;
-    this.selected_file = <File>event.target.files[0]
+    this.selected_file = <File>event.target.files[0];
   }
   goBack(): void {
     this.location.back();
