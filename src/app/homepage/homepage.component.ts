9056e74a012f1a2fb335a600e88e2ff2d0232650
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-04-11 15:11:24 +0200

    first name greeting

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,23 +9,32 @@ import { Location } from '@angular/common';
   styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
+  userName!: any;
 
-
-  constructor(private getData: GetdataService, private authService: AuthService
-    , private location: Location) { }
+  constructor(
+    private getData: GetdataService,
+    private authService: AuthService,
+    private location: Location
+  ) {
+    const credentials = this.authService.getCredentials();
+    const nameArray = credentials?.naam?.toLowerCase().split(' ');
+    this.userName =
+      nameArray?.[0]?.charAt(0).toUpperCase() + nameArray?.[0]?.slice(1);
+  }
 
   ngOnInit(): void {
-    document.body.style.backgroundImage = "url('https://i.postimg.cc/8NqcDrfY/Default-Wallpaper.png')";
+    document.body.style.backgroundImage =
+      "url('https://i.postimg.cc/8NqcDrfY/Default-Wallpaper.png')";
   }
   logOut(): void {
-    var cookies = document.cookie.split(";");
+    var cookies = document.cookie.split(';');
 
     // Loop through each cookie and delete it
     for (var i = 0; i < cookies.length; i++) {
       var cookie = cookies[i];
-      var eqPos = cookie.indexOf("=");
+      var eqPos = cookie.indexOf('=');
       var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/';
     }
     //this.location.back();
     // Clear local storage
