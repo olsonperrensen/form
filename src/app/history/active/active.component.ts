69d3eb24a1442f0751454936864ca3b99cb9c5a2
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-04-17 12:53:00 +0200

    fix timestamp since invoice sent

--- src/app/history/active/active.component.ts
+++ src/app/history/active/active.component.ts
@@ -104,15 +104,34 @@ export class ActiveComponent implements OnInit, AfterViewInit {
     this.location.back();
   }
   timeSinceInvoice(invoiceDate: string): string {
-    const invoiceDateTime = new Date(invoiceDate);
+    if (invoiceDate === 'Pending') {
+      return invoiceDate;
+    }
+
+    const parts = invoiceDate.split(' ');
+    let dateTimeString = '';
+
+    // Check if the input contains "Sent to AP at"
+    if (parts.includes('Sent')) {
+      // Extract the date and time from the input
+      const dateTimeIndex = parts.indexOf('at') + 1;
+      dateTimeString = parts.slice(dateTimeIndex).join(' ');
+    } else {
+      // Assume the input is just the date
+      dateTimeString = invoiceDate;
+    }
+
+    const invoiceDateTime = new Date(dateTimeString);
     const currentDateTime = new Date();
     const timeDiff = currentDateTime.getTime() - invoiceDateTime.getTime();
+
     const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
     const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
     return `${days} days, ${hours} hrs, ${minutes} mins, ${seconds} secs`;
   }
 
